d4dd40bcced7851be452f98a3b639ba18020e906
ailthana89 <62210285+ailthana89@users.noreply.github.com>
2020-06-10 15:51:51 -0300

    atualizações quiz

---
+++ aula-03/quiz.js
@@ -0,0 +1,22 @@
+// Arrow Functions
+
+// Question Title
+// 1. Crie uma função utilizando arrow function com dois parâmetros que retorne a soma deles.
+
+const cookie = (bolacha, biscoito) => {
+    return bolacha + biscoito;
+}
+console.log(5 + 10)
+
+//2. Crie uma função chamada retornarBiografia que retorne o objeto abaixo:
+livro = {nome: "Mulheres, raça e classe",
+descrição:"Da mesma forma que gênero é a maneira como a raça é vivida."
+}
+
+let retornarBiografia = (livro) => {
+    console.log(livro)
+}
+
+// 3. Reescreva a função abaixo utilizando
+//Arrow Function 
+
